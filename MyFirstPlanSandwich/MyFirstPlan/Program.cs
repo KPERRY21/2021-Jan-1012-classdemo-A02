01932d71f359663caf43d3a337b3c73aabdeb795
KPERRY21 <77073218+KPERRY21@users.noreply.github.com>
2021-01-13 10:20:02 -0700

    my first plan sandwich

---
+++ MyFirstPlanSandwich/MyFirstPlan/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyFirstPlan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the Writeline() command will output the 
+            // to the console window.
+            // Steps to make a Loaded Ham Sandwich on White Bread
+            Console.WriteLine("Item List to prepare.");
+            Console.WriteLine("Making a Loaded Ham Sandwich on White Bread.");
+            Console.WriteLine("Bread 2 Slices.");
+            Console.WriteLine("Romaine Lettuce 2 Leaves.");
+            Console.WriteLine("Honey Ham 2 Slices.");
+            Console.WriteLine("Yellow Mustard 1 TBS.");
+            Console.WriteLine("Mayonaise 1 TBS.");
+            Console.WriteLine("Dill Sandwich Pickles 4 Small or 2 Large.");
+            Console.WriteLine("Measuring spoon.");
+            Console.WriteLine("Butter Knife.");
+            Console.WriteLine("Plate.");
+            // How to assemble the Sandwich
+            Console.WriteLine("Place bread on the Plate beside eachother.");
+            Console.WriteLine("Spread Mustard evenly on one slice of bread.");
+            Console.WriteLine("Spread Mayonaise evenly on the remaining peice of bread.");
+            Console.WriteLine("Place Romaine Lettuce the bread on the left.");
+            Console.WriteLine("Place Honey Ham on the bread on the right.");
+            Console.WriteLine("Place Pickles onto Honey Ham.");
+            Console.WriteLine("Hold items in place while you grab the bread from underneath.");
+            Console.WriteLine("Carefully place bread ontop of other piece of sandwich.");
+            Console.WriteLine("Press down gentle and slice sandwich into two equal slices.");
+            Console.WriteLine("Eat Sandwich and Enjoy.");
+        }
+    }
+}
