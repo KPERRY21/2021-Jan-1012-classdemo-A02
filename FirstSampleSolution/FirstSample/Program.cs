64af1a86d25f12129221330fb04abdc4d4685538
KPERRY21 <77073218+KPERRY21@users.noreply.github.com>
2021-01-13 10:16:12 -0700

    create first sample console program

---
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the Writeline() command will output the string //the to the console window.
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This course section is:\t OA03 ");
+            Console.WriteLine("Your instructor name is:\t Don Welch ");
+        }
+    }
+}
